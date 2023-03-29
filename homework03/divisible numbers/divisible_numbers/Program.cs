1364b9db86e6afa23579a637e79d68face0d3fd9
QUVONCHBEK ABDUROZIQOV <75139080+abduroziqov@users.noreply.github.com>
2023-03-29 12:25:10 +0300

    divisible [0,100]

---
+++ homework03/divisible numbers/divisible_numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace divisible_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a divisable=> "); int.TryParse(Console.ReadLine(), out int divisible);
+
+            for (int i = 0; i < 101; i++)
+            {
+                if(i % divisible == 0)
+                {
+                    Console.WriteLine($"Dividers {i}");
+                }
+            }
+        }
+    }
+}
